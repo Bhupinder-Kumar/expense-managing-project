328bbdb9cbb314fba5719cf518b19fd9e9b9ae45
Bhupinder Kumar <bhupinderk0511@gmail.com>
2022-09-28 23:16:39 +0530

    seprating Date Component and js logic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
     <Fragment>
       <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.price}</div>
