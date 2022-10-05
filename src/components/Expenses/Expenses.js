1de5be195af6f1a7c91a265d53841ee975851253
Bhupinder Kumar <bhupinderk0511@gmail.com>
2022-10-05 14:21:42 +0530

    changes and learning about state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
-import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import ExpenseItem from '../ExpenseItems/ExpenseItem';
+import Card from '../Cards/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
