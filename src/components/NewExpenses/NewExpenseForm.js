4abff558776628be8f79b91688bc328558f97c30
Bhupinder Kumar <bhupinderk0511@gmail.com>
2022-10-12 06:37:54 +0530

    [form submission]

--- src/components/NewExpenses/NewExpenseForm.js
+++ src/components/NewExpenses/NewExpenseForm.js
@@ -2,12 +2,33 @@ import React, { Fragment, useState } from 'react';
 import './NewExpenseForm.css';
 
 const NewExpenseForm = () => {
-    const [enteredTite, setEnteredTitle] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    // another way to write multipl states --> 
+
+    // const [userInput, setUserInput] = useState ({
+    //     enteredTitle : '',
+    //     setEnteredAmount : '',
+    //     setEnteredDate : '' 
+    // })
+
     const titleHandler = (e) => {
         setEnteredTitle(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle : e.target.value
+        // })
+
+        // another alternative to catting state inside of function
+
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         enteredTitle : e.target.value
+        //     }
+        // })
     }
 
     const amountHandler = e => {
@@ -17,21 +38,36 @@ const NewExpenseForm = () => {
     const dateHandler = e => {
         setEnteredDate(e.target.value);
     }
+
+    const submitHandler = (event) => {
+        event.preventDefault(); 
+
+        const expeseData = {
+            title : enteredTitle,
+            ammount : enteredAmount,
+            date : new Date(enteredDate)
+        }
+
+        console.log(expeseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
     return(
         <Fragment>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type={enteredTite} onChange={titleHandler}/>
+                        <input type="text" value={enteredTitle} onChange={titleHandler}/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type="number" min="0.01" step="0.01" onChange={amountHandler}/>
+                        <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountHandler}/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Date</label>
-                        <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateHandler}/>
+                        <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateHandler}/>
                     </div>
                     <div className="new-expense__actions">
                         <button type="submit">Submit</button>
